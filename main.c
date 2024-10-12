a98c55eeecf614eca14bc3c67e7a1720fb1a68bf
victoorsoaressq <victoorsoaressq@gmail.com>
2024-10-12 19:55:11 -0300

    projeto 1.1

--- main.c
+++ main.c
@@ -4,17 +4,43 @@
 
 // Painel ADMIN
 void admin() {
-    
+    printf("Admin\n");
 }
 // Painel CLIENT
 void client() {
-    
+    printf("Client\n");
 }
 
 // Função Principal
 int main(){
+    int opcao;
 
     while(1) {
+
+        printf("                            1. Admin\n");
+        printf("\n");
+        printf("                            2. Client\n");
+        printf("\n");
+        printf("                            3. Sair\n");
+
+        printf("\n");
+
+        printf("\n    Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                admin();
+                break;
+            case 2:
+                client();
+                break;
+            case 3:
+                printf("Saindo...\n");
+                exit(0);
+            default:
+                printf("Opção inválida!\n");
+        }
 
     }
     return 0;
