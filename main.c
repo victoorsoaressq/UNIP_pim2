472a17a231bc4e477e703a99feb26f60b0524300
victoorsoaressq <victoorsoaressq@gmail.com>
2024-10-12 19:39:05 -0300

    Build

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Painel ADMIN
+void admin() {
+    
+}
+// Painel CLIENT
+void client() {
+    
+}
+
+// Função Principal
+int main(){
+
+    while(1) {
+
+    }
+    return 0;
+}
