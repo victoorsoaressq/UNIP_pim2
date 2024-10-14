1a5c3d4349924fe3e41a6a41f523a9227acbfe54
victoorsoaressq <victoorsoaressq@gmail.com>
2024-10-14 19:13:59 -0300

    Build 2.1

--- main.c
+++ main.c
@@ -5,16 +5,18 @@
 #define MAX_PRODUCTS 100 // Limite de produtos no estoque
 #define MAX_NAME 50      // Tamanho máximo do nome do produto
 #define MAX_TYPE 20      // Tamanho máximo do tipo do produto
-#define MAX_CART 50   // Limite de itens no carrinho
+#define MAX_CART 50      // Limite de itens no carrinho
 
-typedef struct {
+typedef struct
+{
     char name[MAX_NAME];
     float price;
     int amount;
     char type[MAX_TYPE]; // Novo campo para armazenar o tipo do produto
 } Product;
 
-typedef struct {
+typedef struct
+{
     Product product;
     int amount; // Quantidade do produto que foi comprada
 } CarItem;
@@ -27,34 +29,36 @@ int numProducts = 0; // Número de produtos cadastrados
 CarItem cart[MAX_CART];
 int numCarIten = 0; // Número de itens no carrinho
 
-
-void headerServidor() {
+void headerServidor()
+{
     system("clear");
     printf("--------------------------------------------------------------------------------\n");
     printf("        Servidor                                                                \n");
     printf("--------------------------------------------------------------------------------\n");
 }
 
-//Funcao cadastro de produtos
-void toRegister(){
+// Funcao cadastro de produtos
+void toRegister()
+{
 
     int option;
     int typeOption;
 
-headerServidor();
- if (numProducts >= MAX_PRODUCTS) {
+    headerServidor();
+    if (numProducts >= MAX_PRODUCTS)
+    {
         printf("Estoque cheio! Não é possível cadastrar mais produtos.\n");
         return;
     }
 
- Product newProduct;
-// Recebendo o nome do produto
+    Product newProduct;
+    // Recebendo o nome do produto
     printf("Digite o nome do produto: ");
     getchar(); // Limpa o buffer antes de usar fgets
     fgets(newProduct.name, MAX_NAME, stdin);
     newProduct.name[strcspn(newProduct.name, "\n")] = '\0'; // Remove o '\n' do final
 
-// Recebendo o tipo do produto
+    // Recebendo o tipo do produto
     printf("Escolha o tipo do produto:\n");
     printf("1. Fruta\n");
     printf("2. Legume\n");
@@ -62,19 +66,20 @@ headerServidor();
     printf("Opção: ");
     scanf("%d", &typeOption);
 
-    switch (typeOption) {
-        case 1:
-            strcpy(newProduct.type, "Fruta");
-            break;
-        case 2:
-            strcpy(newProduct.type, "Legume");
-            break;
-        case 3:
-            strcpy(newProduct.type, "Verdura");
-            break;
-        default:
-            printf("Opção inválida! Definindo como 'Outro'.\n");
-            strcpy(newProduct.type, "Outro");
+    switch (typeOption)
+    {
+    case 1:
+        strcpy(newProduct.type, "Fruta");
+        break;
+    case 2:
+        strcpy(newProduct.type, "Legume");
+        break;
+    case 3:
+        strcpy(newProduct.type, "Verdura");
+        break;
+    default:
+        printf("Opção inválida! Definindo como 'Outro'.\n");
+        strcpy(newProduct.type, "Outro");
     }
 
     // Recebendo o preço do produto
@@ -102,28 +107,26 @@ headerServidor();
     printf("Escolha um opção: ");
     scanf("%d", &option);
 
-    if(option == 1 ){ 
+    if (option == 1)
+    {
         toRegister();
-    }else if (option == 2){
+    }
+    else if (option == 2)
+    {
         return;
     }
-
-
 }
 
-
-
-
-void stock(){
+// Função Estoque
+void stock()
+{
 
     int option;
 
- if (numProducts == 0){
+    if (numProducts == 0)
+    {
 
-
-
-
-    headerServidor();
+        headerServidor();
         printf(" Estoque vazio.");
         printf("\n");
         printf("\n");
@@ -132,87 +135,99 @@ void stock(){
         printf("Digite a opção: ");
         scanf("%d", &option);
 
-    if (option != 1) return;
-    
- }else{
+        if (option != 1)
+            return;
+    }
+    else
+    {
 
-    headerServidor();
+        headerServidor();
 
         for (int i = 0; i < numProducts; i++)
-    {
-     printf("Produto %d:\n", i + 1);
+        {
+            printf("Produto %d:\n", i + 1);
             printf("Nome: %s\n", estoque[i].name);
             printf("Tipo: %s\n", estoque[i].type);
             printf("Preço: %.2f\n", estoque[i].price);
             printf("Quantidade: %d\n", estoque[i].amount);
             printf("---------------------------\n");
-    }
-    printf("\n");
+        }
+        printf("\n");
         printf("\n");
         printf("Digite 0 para retornar ao início.");
         printf("\n");
         printf("Digite a opção: ");
         scanf("%d", &option);
 
-    if (option != 1) return;
-
-    
- }
- 
+        if (option != 1)
+            return;
+    }
 }
-
 
 // Painel ADMIN
-void admin() {
+void admin()
+{
 
-int option;
+    int option;
 
+    headerServidor();
+    printf("\n");
+    printf("\n");
 
-headerServidor();
+    printf("        ███████ ███████ ██████  ██    ██ ██ ██████   ██████  ██████   \n");
+    printf("        ██      ██      ██   ██ ██    ██ ██ ██   ██ ██    ██ ██   ██  \n");
+    printf("        ███████ █████   ██████  ██    ██ ██ ██   ██ ██    ██ ██████   \n");
+    printf("             ██ ██      ██   ██  ██  ██  ██ ██   ██ ██    ██ ██   ██  \n");
+    printf("        ███████ ███████ ██   ██   ████   ██ ██████   ██████  ██   ██  \n");
 
-        printf("                            1. Cadastro de produtos\n");
-        printf("\n");
-        printf("                            2. Estoque\n");
-        printf("\n");
-        printf("                            3. Sair\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
 
-        printf("\n");
+    printf("                            1. Cadastro de produtos\n");
+    printf("\n");
+    printf("                            2. Estoque\n");
+    printf("\n");
+    printf("                            3. Sair\n");
 
-        printf("\n    Escolha uma opção: ");
-        scanf("%d", &option);
+    printf("\n");
 
-        switch (option) {
-            case 1:
-                toRegister();
-                break;
-            case 2:
-                stock();
-                break;
-            case 4:
-                printf("Saindo...\n");
-                exit(0);
-            default:
-                printf("Opção inválida!\n");
+    printf("\n    Escolha uma opção: ");
+    scanf("%d", &option);
 
-        }
-
+    switch (option)
+    {
+    case 1:
+        toRegister();
+        break;
+    case 2:
+        stock();
+        break;
+    case 3:
+        printf("Saindo...\n");
+        exit(0);
+    default:
+        printf("Opção inválida!\n");
+    }
 }
 
-// Painel CLIENT
 // Função Header
-void headerClient() {
+void headerClient()
+{
     system("clear");
     printf("--------------------------------------------------------------------------------\n");
     printf("        Caixa                                                                \n");
     printf("--------------------------------------------------------------------------------\n");
 }
 
-void showToCart() {
+void showToCart()
+{
 
-int option;
+    int option;
 
     printf("\n=========== Carrinho de Compras ===========\n");
-    if (numCarIten == 0) {
+    if (numCarIten == 0)
+    {
         printf("Carrinho vazio!\n");
         printf("\n");
         printf("\n");
@@ -221,10 +236,13 @@ int option;
         printf("Digite a opção: ");
         scanf("%d", &option);
 
-    if (option != 1) return;
-
-    } else {
-        for (int i = 0; i < numCarIten; i++) {
+        if (option != 1)
+            return;
+    }
+    else
+    {
+        for (int i = 0; i < numCarIten; i++)
+        {
             printf("Item %d:\n", i + 1);
             printf("Nome: %s\n", cart[i].product.name);
             printf("Preço: %.2f\n", cart[i].product.price);
@@ -238,16 +256,18 @@ int option;
         printf("Digite a opção: ");
         scanf("%d", &option);
 
-    if (option != 1) return;
-
+        if (option != 1)
+            return;
     }
 }
 
 // Função para adicionar um item ao carrinho
-void addToCart(Product product, int amount) {
-int option;
+void addToCart(Product product, int amount)
+{
+    int option;
 
-    if (numCarIten >= MAX_CART) {
+    if (numCarIten >= MAX_CART)
+    {
         printf("Carrinho cheio! Não é possível adicionar mais itens.\n");
         return;
     }
@@ -271,20 +291,23 @@ int option;
     printf("Escolha um opção: ");
     scanf("%d", &option);
 
-    if(option == 1 ){ 
-        return;
-    }else if (option == 2){
+    if (option == 1)
+    {
         return;
     }
-
+    else if (option == 2)
+    {
+        return;
+    }
 }
 
 // Função Comprar Produto
-void buyProduct() {
-int typeOptions;
-int productOption;
-int amount;
-    Product *productAvailable [MAX_PRODUCTS];
+void buyProduct()
+{
+    int typeOptions;
+    int productOption;
+    int amount;
+    Product *productAvailable[MAX_PRODUCTS];
     int numAvailable = 0;
 
     // Menu de escolha do tipo de produto
@@ -296,34 +319,39 @@ int amount;
     scanf("%d", &typeOptions);
 
     // Filtra os produtos no estoque com base no tipo escolhido
-    for (int i = 0; i < numProducts; i++) {
-        if ((typeOptions== 1 && strcmp(estoque[i].type, "Fruta") == 0) ||
-            (typeOptions== 2 && strcmp(estoque[i].type, "Legume") == 0) ||
-            (typeOptions == 3 && strcmp(estoque[i].type, "Verdura") == 0)) {
+    for (int i = 0; i < numProducts; i++)
+    {
+        if ((typeOptions == 1 && strcmp(estoque[i].type, "Fruta") == 0) ||
+            (typeOptions == 2 && strcmp(estoque[i].type, "Legume") == 0) ||
+            (typeOptions == 3 && strcmp(estoque[i].type, "Verdura") == 0))
+        {
             productAvailable[numAvailable] = &estoque[i];
             numAvailable++;
         }
     }
 
     // Exibe os produtos disponíveis
-    if (numAvailable == 0) {
+    if (numAvailable == 0)
+    {
         printf("Não há produtos disponíveis para essa categoria.\n");
         return;
     }
 
     printf("\nProdutos disponíveis:\n");
-    for (int i = 0; i < numAvailable; i++) {
-        printf("%d. %s (Preço: %.2f, Quantidade em estoque: %d)\n", 
-               i + 1, 
-               productAvailable[i]->name, 
-               productAvailable[i]->price, 
+    for (int i = 0; i < numAvailable; i++)
+    {
+        printf("%d. %s (Preço: %.2f, Quantidade em estoque: %d)\n",
+               i + 1,
+               productAvailable[i]->name,
+               productAvailable[i]->price,
                productAvailable[i]->amount);
     }
 
     // Escolha do produto
     printf("Escolha um produto (número): ");
     scanf("%d", &productOption);
-    if (productOption < 1 || productOption > numAvailable) {
+    if (productOption < 1 || productOption > numAvailable)
+    {
         printf("Opção inválida!\n");
         return;
     }
@@ -336,7 +364,8 @@ int amount;
     printf("\n");
 
     // Verifica se há estoque suficiente
-    if (amount > selectProduct->amount) {
+    if (amount > selectProduct->amount)
+    {
         printf("Quantidade indisponível em estoque!\n");
         printf("\n");
         return;
@@ -347,22 +376,23 @@ int amount;
     addToCart(*selectProduct, amount);
 }
 
-
-// Função Realizar Pagamento
 // Função para exibir e pagar os itens do carrinho
-void toPay() {
+void toPay()
+{
 
     int option;
     float total = 0;
 
-    if (numCarIten == 0) {
+    if (numCarIten == 0)
+    {
         printf("Carrinho vazio! Não há nada para pagar.\n");
         return;
     }
 
     printf("\n=========== Pagamento ===========\n");
     printf("Itens no carrinho:\n");
-    for (int i = 0; i < numCarIten; i++) {
+    for (int i = 0; i < numCarIten; i++)
+    {
         printf("%d. %s - Quantidade: %d - Preço unitário: %.2f - Total: %.2f\n",
                i + 1,
                cart[i].product.name,
@@ -376,13 +406,14 @@ void toPay() {
     printf("\n");
     printf("\nDeseja pagar? (1-Sim / 2-Não): ");
     printf("\n");
-    
 
     scanf("%d", &option);
 
-    if (option == 1) {
+    if (option == 1)
+    {
         printf("\n========= Nota Fiscal =========\n");
-        for (int i = 0; i < numCarIten; i++) {
+        for (int i = 0; i < numCarIten; i++)
+        {
             printf("%d. %s - Quantidade: %d - Preço unitário: %.2f - Total: %.2f\n",
                    i + 1,
                    cart[i].product.name,
@@ -409,22 +440,39 @@ void toPay() {
         printf("Escolha um opção: ");
         scanf("%d", &option);
 
-    if(option == 1 ){ 
-        return;
-    }else if (option == 2){
-        return;
+        if (option == 1)
+        {
+            return;
+        }
+        else if (option == 2)
+        {
+            return;
+        }
     }
-    } else {
+    else
+    {
         printf("Pagamento cancelado.\n");
     }
 }
 
 // Painel CLIENT
-void client() {
+void client()
+{
     int option;
 
-    while(1) {
+    while (1)
+    {
         headerClient();
+        printf("\n");
+
+        printf("                        ██████  █████  ██ ██   ██  █████  \n");
+        printf("                       ██      ██   ██ ██  ██ ██  ██   ██ \n");
+        printf("                       ██      ███████ ██   ███   ███████ \n");
+        printf("                       ██      ██   ██ ██  ██ ██  ██   ██ \n");
+        printf("                        ██████ ██   ██ ██ ██   ██ ██   ██ \n");
+
+        printf("\n");
+        printf("\n");
 
         printf("\n");
         printf("\n");
@@ -441,64 +489,78 @@ void client() {
         printf("\n    Escolha uma opção: ");
         scanf("%d", &option);
 
-        switch (option) {
-            case 1:
-                buyProduct();
-                break;
-            case 2:
-                showToCart();
-                break;
-            case 3:
-                toPay();
-                break;   
-            case 4:
+        switch (option)
+        {
+        case 1:
+            buyProduct();
+            break;
+        case 2:
+            showToCart();
+            break;
+        case 3:
+            toPay();
+            break;
+        case 4:
             return;
             break;
-            default:
-                printf("Opção inválida!\n");
+        default:
+            printf("Opção inválida!\n");
         }
-
     }
-  
 }
-// Função Principal
-int main(){
 
+// Função Principal
+int main()
+{
     int option;
 
-system("clear");
+    system("clear");
+    printf("\n");
+    printf("\n");
 
-printf("\n");
-printf("\n");
-printf("\n");
+    printf("   ██   ██  ██████  ██████  ████████ ██ ███████ ██████  ██    ██ ████████ ██ \n");
+    printf("   ██   ██ ██    ██ ██   ██    ██    ██ ██      ██   ██ ██    ██    ██    ██ \n");
+    printf("   ███████ ██    ██ ██████     ██    ██ █████   ██████  ██    ██    ██    ██ \n");
+    printf("   ██   ██ ██    ██ ██   ██    ██    ██ ██      ██   ██ ██    ██    ██    ██ \n");
+    printf("   ██   ██  ██████  ██   ██    ██    ██ ██      ██   ██  ██████     ██    ██ \n");
 
-    while(1) {
+    printf("\n");
+    printf("\n");
+    printf("\n");
 
-        printf("                            1. Servidor\n");
+    while (1)
+    {
         printf("\n");
-        printf("                            2. Caixa\n");
-        printf("\n");
-        printf("                            3. Sair\n");
 
+        printf("                               1. Servidor\n");
+        printf("\n");
+        printf("                               2. Caixa\n");
+        printf("\n");
+        printf("                               3. Sair\n");
+
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
         printf("\n");
 
         printf("\n    Escolha uma opção: ");
         scanf("%d", &option);
 
-        switch (option) {
-            case 1:
-                admin();
-                break;
-            case 2:
-                client();
-                break;
-            case 3:
-                printf("Saindo...\n");
-                exit(0);
-            default:
-                printf("Opção inválida!\n");
+        switch (option)
+        {
+        case 1:
+            admin();
+            break;
+        case 2:
+            client();
+            break;
+        case 3:
+            printf("Saindo...\n");
+            exit(0);
+        default:
+            printf("Opção inválida!\n");
         }
-
     }
     return 0;
 }
